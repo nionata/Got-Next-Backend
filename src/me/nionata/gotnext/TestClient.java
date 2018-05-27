5fad15f88029b902e5ba95f431c580b7a7b5a081
Nicholas Ionata <n.ionata129@gmail.com>
2018-05-27 19:38:26 -0400

    Finished test client

--- src/me/nionata/gotnext/TestClient.java
+++ src/me/nionata/gotnext/TestClient.java
@@ -1,35 +1,34 @@
 package me.nionata.gotnext;
 
-import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class TestClient {
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
+		run();
+	}
+	
+	public static void run() throws Exception {
 		Scanner consoleIn = new Scanner(System.in);
-		Socket sock;
-		Scanner sockIn;
-		PrintStream p;
+		Socket socket = new Socket("localhost", 1291);
+		Scanner sockIn = new Scanner(socket.getInputStream());
+		PrintStream sockOut = new PrintStream(socket.getOutputStream());
 		
-		try {
-			sock = new Socket("localhost", 1291);
-			sockIn = new Scanner(sock.getInputStream());
-			p = new PrintStream(sock.getOutputStream());
+		int number;
+		do {
+			System.out.print("Enter any number: ");
+			number = consoleIn.nextInt();
+			sockOut.println(number);
 			
-			System.out.println("Enter any number");
-			int number = consoleIn.nextInt();
-
-			p.println(number);
 			int temp = sockIn.nextInt();
-			System.out.println(temp);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+			System.out.println(temp + "\n");
+		} while (number != 0);
 		
 		consoleIn.close();
+		socket.close();
+		sockIn.close();
 	}
 
 }
